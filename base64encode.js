49c5c4854bf84eb7c9e5d36c90eb5aea42dc6393
JayTheFoxDemon <32374227+JayTheFoxDemon@users.noreply.github.com>
2018-03-15 13:53:37 -0400

    Delete base64encode.js

--- base64encode.js
+++
@@ -1,6 +0,0 @@
-const Discord = require("discord.js");
-exports.run = (client, message, args) => {
-    let msg = args.slice(0).join(" ");
-    if (!args[0]) return message.reply("Give me something to encode to base64!");
-    message.channel.send(Buffer.from(msg).toString('base64'));
-};
